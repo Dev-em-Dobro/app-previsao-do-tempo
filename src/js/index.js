b57a89a08abf53be88be7ded306d917b2ca41e02
devemdobro <devemdobro@gmail.com>
2023-11-20 18:38:58 -0300

    ajustes pauta

--- src/js/index.js
+++ src/js/index.js
@@ -1,35 +1,44 @@
-const chaveDeApi = "d97c2c60a040471a986221219230611";
+const chaveDeApi = "cd31863ae267407cb01212131232011";
+const botaoDeBusca = document.querySelector(".btn-busca");
 
-async function buscarDadosDeClima(cidade) {
-  const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${chaveDeApi}&q=${cidade ? cidade : "Porto Alegre"}&lang=pt`;
-  const resposta = await fetch(apiUrl);
-  const dados = await resposta.json();
+botaoDeBusca.addEventListener("click", async () => {
+  const cidade = document.getElementById("input-busca").value;
 
-  return dados; 
-}
+  if (!cidade) return;
 
-document.querySelector(".btn-busca").addEventListener("click", async () => {
-    const cidade = document.getElementById("busca").value;
-    const dados = await buscarDadosDeClima(cidade);
-    preencherDadosNaTela(dados, cidade);
+  const dados = await buscarDadosDeClima(cidade);
+
+  if (dados) preencherDadosNaTela(dados, cidade);
 });
 
-function preencherDadosNaTela(dados, cidade){
-    const tempCelsius = dados.current.temp_c;
-    const humidade = dados.current.humidity;
-    const condicao = dados.current.condition.text;
-    const velocidadeDoVento = dados.current.wind_kph;
-    const iconeCondicao =  dados.current.condition.icon;
+async function buscarDadosDeClima(cidade) {
+  const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${chaveDeApi}&q=${cidade}&aqi=no&lang=pt`;
+  const resposta = await fetch(apiUrl);
 
-    document.getElementById("cidade").textContent = cidade;
-    document.getElementById("numero-temperatura").textContent = tempCelsius + " °C";
-    document.getElementById("humidade").textContent = humidade + "%";
-    document.getElementById("condicao").textContent = condicao;
-    document.getElementById("velocidade-do-vento").textContent = velocidadeDoVento + " km/h";
-    document.getElementById("icone-condicao").setAttribute("src", iconeCondicao);
-    
-    document.getElementById("container").classList.add(dados.current.condition.text)
+  console.log(resposta.status);
 
+  if (resposta.status !== 200) {
+    return;
+  }
 
+  const dados = await resposta.json();
+
+  return dados;
 }
 
+function preencherDadosNaTela(dados, cidade) {
+  const tempCelsius = dados.current.temp_c;
+  const humidade = dados.current.humidity;
+  const condicao = dados.current.condition.text;
+  const velocidadeDoVento = dados.current.wind_kph;
+  const iconeCondicao = dados.current.condition.icon;
+
+  document.getElementById("cidade").textContent = cidade;
+  document.getElementById("numero-temperatura").textContent =
+    tempCelsius + " °C";
+  document.getElementById("humidade").textContent = humidade + "%";
+  document.getElementById("condicao").textContent = condicao;
+  document.getElementById("velocidade-do-vento").textContent =
+    velocidadeDoVento + " km/h";
+  document.getElementById("icone-condicao").setAttribute("src", iconeCondicao);
+}
