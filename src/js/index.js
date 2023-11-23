bebd9f1875bd12868e05f42a173677f36d1f35c9
devemdobro <devemdobro@gmail.com>
2023-11-22 23:15:00 -0300

    correções live

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
-const chaveDeApi = "cd31863ae267407cb01212131232011";
+// coloque aqui a sua chave de api
+const chaveDaApi = "cd31863ae267407cb01212131232011";
+
 const botaoDeBusca = document.querySelector(".btn-busca");
 
 botaoDeBusca.addEventListener("click", async () => {
@@ -6,39 +8,41 @@ botaoDeBusca.addEventListener("click", async () => {
 
   if (!cidade) return;
 
-  const dados = await buscarDadosDeClima(cidade);
+  const dados = await buscarDadosDaCidade(cidade);
 
   if (dados) preencherDadosNaTela(dados, cidade);
 });
 
-async function buscarDadosDeClima(cidade) {
-  const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${chaveDeApi}&q=${cidade}&aqi=no&lang=pt`;
+async function buscarDadosDaCidade(cidade) {
+  const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${chaveDaApi}&q=${cidade}&aqi=no&lang=pt`;
+
   const resposta = await fetch(apiUrl);
 
-  console.log(resposta.status);
+  if (resposta.status !== 200) return;
 
-  if (resposta.status !== 200) {
-    return;
-  }
-
-  const dados = await resposta.json();
+  const dados = resposta.json();
 
   return dados;
 }
 
 function preencherDadosNaTela(dados, cidade) {
-  const tempCelsius = dados.current.temp_c;
-  const humidade = dados.current.humidity;
+  const temperatura = dados.current.temp_c;
   const condicao = dados.current.condition.text;
+  const humidade = dados.current.humidity;
   const velocidadeDoVento = dados.current.wind_kph;
   const iconeCondicao = dados.current.condition.icon;
 
   document.getElementById("cidade").textContent = cidade;
-  document.getElementById("numero-temperatura").textContent =
-    tempCelsius + " °C";
-  document.getElementById("humidade").textContent = humidade + "%";
+
+  document.getElementById("temperatura").textContent = `${temperatura} ºC`;
+
   document.getElementById("condicao").textContent = condicao;
-  document.getElementById("velocidade-do-vento").textContent =
-    velocidadeDoVento + " km/h";
+
+  document.getElementById("humidade").textContent = `${humidade}%`;
+
+  document.getElementById(
+    "velocidade-do-vento"
+  ).textContent = `${velocidadeDoVento} km/h`;
+
   document.getElementById("icone-condicao").setAttribute("src", iconeCondicao);
 }
